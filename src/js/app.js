7f3ae4d8607bec2e4efeea536afbf1a2412aa4ec
Victor_Nov <victornov@bk.ru>
2022-04-07 23:45:08 +0500

    Добавлена функция переключения темы

--- src/js/app.js
+++ src/js/app.js
@@ -72,12 +72,61 @@ function finalVideo() {
 
 videoFile.addEventListener('ended', finalVideo)
 
+// Show Scroll Up
+function showScroll() {
+  const scrollUp = document.getElementById('scroll-up')
+  if (this.scrollY >= window.innerHeight) {
+    scrollUp.classList.add('show-scroll')
+  } else {
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+window.addEventListener('scroll', showScroll)
 
+// Отметить ссылку на активную секцию
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+  const scrollY = window.scrollY
 
+  sections.forEach(section => {
+    const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop - 50
+    const sectionId = section.getAttribute('id')
 
+    if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+      document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link')
+    } else {
+      document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove('active-link')
+    }
+  })
+}
 
+window.addEventListener('scroll', scrollActive)
 
+// Переключение темная/светлая темы
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-sun-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 
 
